d0ffb1a2c04b40382ff62bdb0f7a1829639f71b8
lalithasrisrbmo <lalithasrisr@gmail.com>
2019-07-30 09:51:20 -0400

    'commit'

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/resturantlistapp'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + 'dist/index.html'));
+    res.sendFile(path.join(__dirname + 'dist/resturantlistapp/index.html'));
 })
 
 console.log('Console listening!');
